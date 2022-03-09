3666f1476dc32ca30037d81d22583880f9cdfe2a
Esanov <94538099+Zukhriddin-27@users.noreply.github.com>
2022-03-09 10:54:06 +0500

    new

--- src/component/common/Logo/index.jsx
+++ src/component/common/Logo/index.jsx
@@ -6,9 +6,11 @@ const Logo = () => {
     <div className='logo'>
       <div>
         <img src={logoImg} alt='Logo-img' />
-        <span>
-          <b>Taste</b>
-        </span>
+        <div>
+          <b>
+            Taste<span>zez</span>
+          </b>
+        </div>
       </div>
       <p>Restaurant & BBQ</p>
     </div>
